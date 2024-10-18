8b47fc2b06344700bff9eb11e022d4ce33b88f8b
Bertrand <136034307+ufimoni@users.noreply.github.com>
2024-10-18 13:38:31 +0100

    Update connect.java

--- connect.java
+++ connect.java
@@ -4,6 +4,7 @@ import com.mongodb.client.MongoDatabase;
 
 public class MongoDBConnectionExample {
     public static void main(String[] args) {
+        /// THIS IS BACKEND CODE USING JAVA FOR OUR BACKEND APPLICATION
         // Replace the uri string with your MongoDB deployment's connection string
         String uri = "mongodb+srv://<admin>:<admin123>@cluster0.mongodb.net/test?retryWrites=true&w=majority";
         

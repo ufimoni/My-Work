dada09ca1c0b4fbc5198e2a6f7703c27eece031b
ufimoni <ekongbert02@gmail.com>
2024-10-25 17:49:10 +0100

    react.v1

---
+++ react-files/Test.jsx
@@ -0,0 +1,11 @@
+// our first components
+// const work = 23;
+function displayFood(){
+    return "Chicken and rice"
+}
+function Test(){
+    return <div>
+     <h1>Welcome Bertcode.py you are {20+90},{displayFood()}</h1>
+    </div>;
+}
+export default Test;

b8b0a1a3c928de04e251da3000bc50eb1ea523d7
ufimoni <ekongbert02@gmail.com>
2024-10-25 18:18:27 +0100

    react.v1

--- react-files/App.jsx
+++ react-files/App.jsx
@@ -3,10 +3,12 @@ function App() {
   return (
     <div className="App">
     <Test name="Jojo" age="21" message="Cool this is so much cool" />
+    <Test name="Mary" age="25" message="Type this again mary this awesome" />
+    <Test name="James" age="27" message="keep going up again till the sky is limit" />
    
     </div>
 
   )
 }
 
-export default App
+export default App;

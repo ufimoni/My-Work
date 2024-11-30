7d5082d76303d8852006b346a9e1487f58d9dc4a
ufimoni <ekongbert02@gmail.com>
2024-11-30 10:27:05 +0100

    V3.0.2 Updated code

--- Route/moviesRoute.js
+++ Route/moviesRoute.js
@@ -1,19 +1,33 @@
 
 const express = require('express');
-const router = express.Router();
+
 const movcontrol = require('./../controllers/movieController');
 
+const router = express.Router();
+
+// to get the status.
+router.route('/movies-stats')
+            .get(movcontrol.getMovieStats);
+
+//// to get the genre
+router.route('/movies-genre/:genres')
+        .get(movcontrol.getMovieByGenre);
+// to destructor code and get the spicified feild. 
+//router.route('/Movies/Actors').get(movcontrol.getMovieByGenre);
+
 // params middleware
-router.param('id',movcontrol.checkID);
+//router.param('id',movcontrol.checkID);
 /* router.param('id',(req, res, next, value) =>{
     console.log("Movie ID is :" +value);  // value stores the id
     next();
 })*/
-
+ /// uing the middleware to handle the aliasing of the route
+ router.route('/highest-rated')
+        .get(movcontrol.gethighesrated, movcontrol.getAllmovies);
 
 router.route('/')  // app.route('/api/v1/movies') was change.
     .get(movcontrol.getAllmovies)
-    .post(movcontrol.validbody, movcontrol.createMovie);
+    .post(movcontrol.createMovie);
  
 router.route('/:id')     // app.route('/api/v1/movies/:id')
    .get(movcontrol.getmovie)
@@ -21,4 +35,4 @@ router.route('/:id')     // app.route('/api/v1/movies/:id')
    .delete(movcontrol.deletemovie)
 
 
-   module.exports = router;
+module.exports = router;
